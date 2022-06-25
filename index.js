d987dd2d0891ff4a346145a128955208b8a8a4fe
Usman_M_Kafi <mkafi3000@gmail.com>
2022-06-25 02:12:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let iname = document.getElementById('name')
+let iheight = document.getElementById('height')
+let icountry = document.getElementById('country')
+let myHeight = 1.70
+
+iname.innerHTML = 'My name is: Usman Muhammad Kafi'
+iheight.innerHTML = `My height is: ${myHeight}M`
+icountry.innerHTML = 'I am a Nigerian'
+
